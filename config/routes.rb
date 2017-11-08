5d495ecf08707d361827eb1c5f24b0e69165d48a
afiebig <alfredo.fiebig@continuum.cl>
2017-11-08 14:37:39 -0300

    Put the monitor resource inside complex_example

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   scope 'complex_example' do
     resources :personas, defaults: {format: :json}
+
+    get 'monitor', to: 'monitor#status', defaults: {format: :json}
   end
-
-  get 'monitor', to: 'monitor#status', defaults: {format: :json}
-
+  
 end
