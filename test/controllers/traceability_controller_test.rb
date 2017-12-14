9e13687f00ce24e1852d1825e335bae8e705fe63
Eugenio <eugenionlopez@gmail.com>
2017-12-14 13:23:04 -0300

    remove debugger statement...

--- test/controllers/traceability_controller_test.rb
+++ test/controllers/traceability_controller_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
 class TraceabilityControllerTest < ActionDispatch::IntegrationTest
-  
+
   test "should respond with logs for 2 days" do
     get logs_path, params: {inicio: '2017-12-12', fin: '2018-05-25'}
-    debugger
     assert_equal response.status, 200
     assert_equal "application/gzip", response.content_type
   end
